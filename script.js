6ed85117cd1e710de17dd42c761efc4d6fe6a736
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2023-07-01 08:51:11 -0400

    Set up random choice for the computers play

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getComputerChoice() {
-    const computerChoice = ['Rock','Paper','Scissors']
-    alert(computerChoice[1]) // This is so I want to reference a specific string in the array. This will be rock
+    const computerChoice = ['Rock','Paper','Scissors'];
+    let randomChoice = computerChoice[(Math.floor(Math.random() * computerChoice.length))];
+    console.log(randomChoice)
 }
 
+ // This is so I want to reference a specific string in the array. This will be rock alert(computerChoice[1])
