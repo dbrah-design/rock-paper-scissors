880ec7a8806368f45ad6fd88ba7770b9f555ab09
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2023-07-01 08:29:39 -0400

    Set up a function for computer choice

---
+++ script.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+    const computerChoice = ['Rock','Paper','Scissors']
+    alert(computerChoice[1]) // This is so I want to reference a specific string in the array. This will be rock
+}
+
