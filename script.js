8bc979906d86272be73913fec5f62b8fe12a9357
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-04-06 09:10:18 -0400

    Fixed merging issue

--- script.js
+++ script.js
@@ -1,53 +1,5 @@
 
 function getComputerChoice() {
-<<<<<<< HEAD
-   const computerChoice = ['rock','paper','scissors'];
-   return computerChoice[(Math.floor(Math.random() * computerChoice.length))];
- } 
- 
- let playerScore = 0;
- let computerScore = 0;
- 
- function playRound(playerSelection, computerSelection) {
-   if (playerSelection === computerSelection) {
-    return `It's a tie! you both picked ${playerSelection}`;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerScore++
-    return "You win! Rock beats Scissors";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++
-     return "You win! Paper beats Rock";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++
-     return "You win! Scissors beats Paper";
-  } else {
-     computerScore++;
-     return `You lose! ${computerSelection} beats ${playerSelection}`;
-  }
-    
- }
- 
- /* Code to run one round of the game
- 
- const playerSelection = prompt("What is your selection?").toLowerCase(); 
- const computerSelection = getComputerChoice();
- console.log(playRound(playerSelection, computerSelection));
- console.log(`player score: ${playerScore} computer score: ${computerScore}`)
- */
- 
- function playGame() {
-    for (let i=0;i<5;i++) {
-       const playerSelection = prompt("What is your selection?").toLowerCase(); 
-       const computerSelection = getComputerChoice();
-       console.log(playRound(playerSelection, computerSelection));
-       console.log(`player score: ${playerScore} computer score: ${computerScore}`);
-    }
- }
- 
- 
- 
-  playGame()
-=======
   const computerChoice = ['rock','paper','scissors'];
   return computerChoice[(Math.floor(Math.random() * computerChoice.length))];
 } 
@@ -106,4 +58,3 @@ function playRound(playerSelection) {
    
 }
 
->>>>>>> rps-ui
