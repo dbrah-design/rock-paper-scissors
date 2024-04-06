8cc00363f3d37342aaa5b2d28fb665f6c9102ff2
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2024-04-06 08:24:47 -0400

    Edited text content in display score and removed comments

--- script.js
+++ script.js
@@ -48,36 +48,13 @@ function playRound(playerSelection) {
  }
 
  if (playerScore === 5) {
-  displayWinner.textContent = "You reached 5 points. You win!!!!";
+  displayWinner.textContent = "You reached 5 points. You win the game!!!!";
  } else if(computerScore === 5) {
-  displayWinner.textContent = "Computer reached 5 points. You lose!!!";
+  displayWinner.textContent = "Computer reached 5 points. You lose the game!!!";
  }
  
  displayResult.textContent = result
- displayScore.textContent = `Player Score: ${playerScore} | Computer Score:${computerScore}`;
+ displayScore.textContent = `Player Score: ${playerScore} | Computer Score: ${computerScore}`;
    
 }
 
-
-// Something is off with the logic for the play round function. When I click one of the buttons multiple times, it repeats the same message. Need to figure out why it's doing that. Figured it out. It had to do  with the computer selection. For some reason it kept repeating the computer selection when I had the code written like const computerSelection = getComputerChoice() outside the scope of the event listener. Once I removed it and added getComputerChoice() to playRound function inside event listener it seemed to work.
-
-/* Code to run one round of the game
-
-const playerSelection = prompt("What is your selection?").toLowerCase(); 
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(`player score: ${playerScore} computer score: ${computerScore}`)
-*/
-/* Logic for playing five rounds
-function playGame() {
-   for (let i=0;i<5;i++) {
-      const playerSelection = prompt("What is your selection?").toLowerCase(); 
-      const computerSelection = getComputerChoice();
-      console.log(playRound(playerSelection, computerSelection));
-      console.log(`player score: ${playerScore} computer score: ${computerScore}`);
-   }
-}
-*/
-
-
- // playGame()
